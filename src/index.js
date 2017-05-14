0a2ea0277cc45ab92f5becc58670dac38fbe21ae
Asaf David <asaf.d@fiverr.com>
2017-05-14 08:55:33 +0300

    main functionality and examples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,49 @@
-class FetchLoader extends React.Component {
+import React from 'react';
+const phases = {
+    fetching: 1,
+    success: 2,
+    error: 4
+}
 
+class FetchLoader extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            phase: phases.fetching
+        }
+
+        this.style = {
+            width: props.width || '100%',
+            height: props.height || '100%',
+            textAlign: 'center'
+        }
+    }
+
+    componentDidMount() {
+        this.props.fetch()
+            .then((res) => res.json())
+            .then((data) => {
+                this.data = data;
+                this.setState({ phase: phases.success });
+            })
+            .catch(() => this.setState({ phase: phases.error }))
+    }
+
+    render() {
+        const { phase } = this.state;
+
+        if (phase === phases.success) {
+            return React.cloneElement(this.props.children, {fetchedData: this.data});
+        } else {
+            return (
+                <div style={this.style}>
+                    {phase === phases.fetching && <div>Loading...</div>}
+                    {phase === phases.error && <div>{this.props.errorText}</div>}
+                </div>
+            )
+        }
+    }
 }
 
 export default FetchLoader;
